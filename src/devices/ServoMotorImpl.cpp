ddea2017046548bb115a4ffb48ed4d0993046eed
Federico Mariani - federico.mariani3@studio.unibo.it <federico.mariani3@studio.unibo.it>
2025-11-19 11:54:43 +0100

    created the classes for Servo Motor

---
+++ src/devices/ServoMotorImpl.cpp
@@ -0,0 +1,22 @@
+#include "ServoMotorImpl.h"
+#include <arduino.h>
+
+ServoMotorImpl::ServoMotorImpl(int pin){
+    this->pin = pin;
+}
+
+void on(){
+
+}
+
+void isOn(){
+
+}
+
+void setPosition(int angle){
+
+}
+
+void off(){
+    
+}
